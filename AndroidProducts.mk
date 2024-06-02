802d347d2d43dd8b7c04d955c0be5ad90da5392b
Noptytyd Hehehe <nophygege@gmail.com>
2024-06-02 18:57:53 +0000

    initial device tree

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_X657B.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_X657B-user \
+    lineage_X657B-userdebug \
+    lineage_X657B-eng
